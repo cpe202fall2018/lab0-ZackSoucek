75eeaa4091af6eef99b3177c0d0fbeaa2f171dfa
Zack Soucek <zacksoucek@gmail.com>
2018-09-21 11:01:31 -0700

    done without casting fix
    git

--- planets.py
+++ planets.py
@@ -1,9 +1,9 @@
 def weight_on_planets():
-
-
-    # write your code here
-pass
-    # Github test
+    weight = float(input("What do you weigh on earth? "))
+    jupiterWeight = weight*2.34
+    marsWeight = weight*0.38
+    print("On Mars you would weigh " + marsWeight + " pounds."
+         + "\nOn Jupiter you would weigh" + jupiterWeight + " pounds.")
 
 
 if __name__ == '__main__':
